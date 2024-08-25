5c6b6c41f8f77c2ecfbec7416e76801f38fc0622
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2024-08-26 01:43:54 +0300

    eod

--- blog-api/controllers/skillController.ts
+++ blog-api/controllers/skillController.ts
@@ -6,12 +6,17 @@ import {  Skill } from "@prisma/client";
 const skillsService = new SkillService();
 
 
+// interfaces
 interface SkillRequest extends Request {
     body: Skill;
-  }
+}
+
+interface SkillResponse extends Response {
+    body: Skill;
+}
 
 
-const findAllSkills= async (req: SkillRequest, res: Response) => {
+const findAllSkills = async (req: SkillRequest, res: Response) => {
 
     try {
     
@@ -27,12 +32,57 @@ const findAllSkills= async (req: SkillRequest, res: Response) => {
     } catch (err) {
         // throw new Error(`Error Occurred ${err}`);
         return res.status(404).json({
+            message: "error occured",
+            error: err
+        })
+
+    }
+}
+
+
+const findSkillById = async (req: SkillRequest, res: Response) => {
+
+    try {
+    
+        const id = parseInt(req.params.id);
+
+        const skill = await skillsService.findById(id);
+
+        console.log(skill);
+
+        return res.status(200).send({
+            message: "hello from find all skills",
+            skill: skill
+        });
+
+    } catch (err) {
+        // throw new Error(`Error Occurred ${err}`);
+        return res.status(404).json({
             message: "error occured"
         })
 
     }
 }
 
+const createSkill = async (req: SkillRequest, res: SkillResponse) => {
+
+
+}
+
+
+const updateSkillById = async (req: SkillRequest, res: SkillResponse) => {
+
+
+}
+
+const deleteSkill = async (req: SkillRequest, res: SkillResponse) => {
+
+}
+
 module.exports = {
     findAllSkills,
+    findSkillById,
+    createSkill,
+    updateSkillById,
+    deleteSkill
 }
--- blog-api/routes/skillRoutes.ts
+++ blog-api/routes/skillRoutes.ts
@@ -5,6 +5,11 @@ const skillController = require('../controllers/skillController');
 const router = Router();
 
 router.get('/', skillController.findAllSkills);
+router.get('/', skillController.findSkillById);
+// router.post('/', skillController.createSkill);
+// router.post('/', skillController.createBulkSkill);
+// router.post('/', skillController.deleteSkill);
+// router.post('/', skillController.deleteManySkill);
 
 
 
